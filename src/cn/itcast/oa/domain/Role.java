d4d0353d64e9929293853d5f7bb4eb56145aa6a6
HouJun11220033 <1144724015@qq.com>
2016-08-25 12:07:45 +0800

    privilege实体

--- src/cn/itcast/oa/domain/Role.java
+++ src/cn/itcast/oa/domain/Role.java
@@ -3,17 +3,20 @@ package cn.itcast.oa.domain;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * 岗位
- * 
- * @author tyg
- * 
- */
 public class Role {
+	public Set<Privilege> getPrivileges() {
+		return privileges;
+	}
+
+	public void setPrivileges(Set<Privilege> privileges) {
+		this.privileges = privileges;
+	}
+
 	private Long id;
 	private String name;
 	private String description;
 	private Set<User> users = new HashSet<User>();
+	private Set<Privilege> privileges = new HashSet<Privilege>();
 
 	public Long getId() {
 		return id;
